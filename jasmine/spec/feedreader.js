31bd95cc121e8054b49ba32858a4976a6766e5d7
swaption2009 <swaption2009@gmail.com>
2015-08-03 15:53:32 -0700

    loadFeed async call and no. of entry > 1

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -96,6 +96,7 @@ $(function() {
 
 
     /* TODO: Write a new test suite named "Initial Entries" */
+    describe('Initial Entries', function() {
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -103,6 +104,20 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test wil require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+
+        // set beforeEach function for async support
+        beforeEach(function(done) {
+            loadFeed(0, function() {
+                done();
+            });
+        });
+
+        // count number of entries after async call is done
+        it('loadFeed has more than one entry', function(done) {
+            var entries = $('.feed a').children('.entry');
+            expect(entries.length).toBeGreaterThan(0);
+            done();
+        });
 
 
     /* TODO: Write a new test suite named "New Feed Selection"
@@ -112,5 +127,6 @@ $(function() {
          * Remember, loadFeed() is asynchronous.
          */
 
+    });
 
 }());
